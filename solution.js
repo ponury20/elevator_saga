9cceb32535316e28cb6653b0ee60274c97749584
Jan Kowalski <napiszdoernesta@gmail.com>
2015-11-09 20:07:04 +0100

    challenge #1

--- solution.js
+++ solution.js
@@ -1,14 +1,21 @@
+var foo = 
 {
+	
+	initElevator: function(elevator) {
+		function floorBtnPressed(floorNum) {
+			this.goToFloor(floorNum);
+		};
+		
+		elevator.on("floor_button_pressed", floorBtnPressed);
+	},
+	
+	
+		
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
-
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-        });
+       elevators.forEach(this.initElevator);
     },
+    
+    
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
