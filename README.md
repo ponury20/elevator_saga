e8cbe4ab0f06c885120ac996d93fe2834b27fa2b
Jan Kowalski <napiszdoernesta@gmail.com>
2015-11-13 11:16:47 +0100

    challenge #2 86%

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # elevator_saga
 *Simple javascript solutions to the elevator saga (http://play.elevatorsaga.com/)*
 
-When button on elevator is pressed - go to the requested floor
-
-When elevator is idle - go to the ground floor
+* When somebody press a button on the elevator - go to that floor (if not already in elevator queue). 
+* When passing a floor - check if floor is on queue with the same direction as the elevator. 
+* When elevator is idle - find any calling floor (start from calling to go down). 
+* When elevator stopped at floor - clear (if needed) queues of calling floors.
+* When somebody calls elevator on the floor - put floor to proper queue and call an idle elevator 
+(if no elevator is currently going to that floor)
 
 ## challenge #1
 
-**Efficiency = 95% (95/100)**
+**Efficiency = 100% (100/100)**
 
 ## challenge #2
 
-**Efficiency << 90% (fail)**
+**Efficiency = 86% (86/100) (fail)**
--- solution.js
+++ solution.js
@@ -1,38 +1,127 @@
-var foo = 
-{
-	GROUND_FLOOR: 0,
-	
-	initApp: function(elevators, floors) {
-		GROUND_FLOOR = floors[0].floorNum();
+function {
+
+	initApp : function(elevators, floors) {
+		GROUND_FLOOR = 0;
+		HIGH_FLOOR = floors.lenght - 1;
+		ELEVATORS = elevators;
+		FLOORS = floors;
+		FLOORS_QUEUE_UP = [];
+		FLOORS_QUEUE_DOWN = [];
+
+		UP = "up";
+		DOWN = "down";
 	},
-	
+
 	/**
-	 * Initialize elevator -- add event listeners
-	 * - when somebody enters the elevator, go to floor requested
-	 * - when elevator is idle, go to the ground floor
+	 * When somebody press a button on the elevator - go to that floor. When
+	 * passing a floor - check if floor is on queue with the same direction as
+	 * the elevator. When elevator is idle - find any calling floor (start from
+	 * calling to go down). When elevator stopped at floor - clear if needed
+	 * queues of calling floors.
+	 * 
 	 * @param elevator
-	 */		
-	initElevator: function(elevator) {
-		
+	 */
+	initElevator : function(elevator) {
+
 		function floorBtnPressed(floorNum) {
-			this.goToFloor(floorNum);
-		};
-		
+			if (elevator.destinationQueue.indexOf(floorNum) < 0)
+				elevator.goToFloor(floorNum);
+		}
+
+		function stopOnFloorFromQueue(floorNum, direction) {
+			if (elevator.loadFactor() === 1)
+				return;
+			var queue = [];
+			if (direction === UP || floorNum === HIGH_FLOOR) {
+				queue = FLOORS_QUEUE_UP;
+			} else if (direction === DOWN || floorNum === GROUND_FLOOR) {
+				queue = FLOORS_QUEUE_DOWN;
+			}
+			var idx = queue.indexOf(floorNum);
+			if (idx >= 0) {
+				queue.splice(idx, 1);
+				elevator.goToFloor(floorNum, true);
+			}
+		}
+
+		function findAnyCallingFloor() {
+			if (FLOORS_QUEUE_DOWN.length > 0)
+				elevator.goToFloor(FLOORS_QUEUE_DOWN.shift(), true);
+			else if (FLOORS_QUEUE_UP.length > 0)
+				elevator.goToFloor(FLOORS_QUEUE_UP.shift(), true);
+		}
+
 		function handleIdle() {
-			this.goToFloor(GROUND_FLOOR);
-		};
-		
+			findAnyCallingFloor();
+		}
+
+		function stoppedAtFloor(floorNum) {
+			var idx = FLOORS_QUEUE_UP.indexOf(floorNum);
+			if (idx >= 0)
+				FLOORS_QUEUE_UP.splice(idx, 1);
+
+			idx = FLOORS_QUEUE_DOWN.indexOf(floorNum);
+			if (idx >= 0)
+				FLOORS_QUEUE_DOWN.splice(idx, 1);
+		}
+
 		elevator.on("floor_button_pressed", floorBtnPressed);
+		elevator.on("passing_floor", stopOnFloorFromQueue);
 		elevator.on("idle", handleIdle);
+		elevator.on("stopped_at_floor", stoppedAtFloor);
 	},
-		
-    init: function(elevators, floors) {
-       this.initApp(elevators, floors);
-       elevators.forEach(this.initElevator);
-    },
-    
-    
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+
+	/**
+	 * Initialize floor -- add event listeners - when somebody calls elevator
+	 * put floor to proper queue and call an idle elevator if no elevator is
+	 * currently going to that floor
+	 * 
+	 * @param floor
+	 */
+	initFloor : function(floor) {
+
+		function callElevator() {
+			for (i = 0; i < ELEVATORS.length; i++)
+				if (ELEVATORS[i].destinationQueue.indexOf(floor.floorNum) >= 0)
+					return;
+
+			for (i = 0; i < ELEVATORS.length; i++)
+				if (ELEVATORS[i].destinationQueue.length === 0) {
+					ELEVATORS[i].trigger("idle");
+					return;
+				}
+
+			// ELEVATORS[0].trigger("idle");
+
+		}
+
+		function queueUp(floor) {
+			if (FLOORS_QUEUE_UP.indexOf(floor.floorNum()) < 0) {
+				FLOORS_QUEUE_UP.push(floor.floorNum());
+				callElevator();
+			}
+
+		}
+
+		function queueDown(floor) {
+			if (FLOORS_QUEUE_DOWN.indexOf(floor.floorNum()) < 0) {
+				FLOORS_QUEUE_DOWN.push(floor.floorNum());
+				callElevator();
+			}
+
+		}
+
+		floor.on("up_button_pressed", queueUp);
+		floor.on("down_button_pressed", queueDown);
+	},
+
+	init : function(elevators, floors) {
+		this.initApp(elevators, floors);
+		elevators.forEach(this.initElevator);
+		floors.forEach(this.initFloor);
+	},
+
+	update : function(dt, elevators, floors) {
+		// We normally don't need to do anything here
+	}
 }
